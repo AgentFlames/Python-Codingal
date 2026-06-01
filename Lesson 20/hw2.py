1fd8915dd2443d73006bfbd531001c7daf993df8
AgentFlames <yuvrajpirteek@gmail.com>
2026-06-01 17:57:58 +0100

    lesson 21

---
+++ Lesson 20/hw2.py
@@ -0,0 +1,13 @@
+# numbers = (int(input("Enter a number: ")), int(input("Enter a number: ")), int(input("Enter a number: ")), int(input("Enter a number: ")), int(input("Enter a number: ")))
+
+
+# print(numbers)
+
+
+num1 = (int(input("Enter a Number: ")), )
+num2 = (int(input("Enter a Number: ")), )
+num3 = (int(input("Enter a Number: ")), )
+num4 = (int(input("Enter a Number: ")), )
+
+nums = (num1[0], num2[0], num3[0], num4[0],)
+print(nums)
