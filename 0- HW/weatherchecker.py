2f0dc07447d1ccc913c54c9b6d70a315669615aa
AgentFlames <yuvrajpirteek@gmail.com>
2026-07-05 20:59:09 +0100

    new

---
+++ 0- HW/weatherchecker.py
@@ -0,0 +1,9 @@
+temp = int(input("enter the temperature: ").strip())
+if temp >=20:
+    print("it is warm where t-shirt and shorts")
+
+elif 5 < temp <20:
+    print("ok temperature, a light jacket, jumper or hoodie will work")
+
+else:
+    print("it is cold wear a good amount of clothing to keep yourself warm")
